d8896c56759c5c2d97aa0bc68aae38ec06bc341f
bala-amypo <bala@amypo.in>
2025-12-18 06:30:35 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/UserEntity.java
@@ -0,0 +1,63 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class VolunteerProfileEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullname;
+    @Column(unique=true)
+    private String email;
+    private String password;
+    private String role;
+   
+
+    public VolunteerProfileEntity(Long id, String fullname, String email, String phone,String availabilityStatus) {
+        // this.id = id;
+        this.fullname = fullname;
+        this.email = email;
+        this.phone = phone;
+        this.availabilityStatus=availabilityStatus;
+    }
+
+    public VolunteerProfileEntity() {
+        
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getFullname() {
+        return this.fullname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+    public void setAvailabilityStatus(String availabilityStatus) {
+        this.availabilityStatus = availabilityStatus;
+    }
+
+    public String getAvailabilityStatus() {
+        return this.availabilityStatus;
+    }
+}
