7780d73dc336508c0872fe1413c694069828877d
bala-amypo <bala@amypo.in>
2025-12-18 06:10:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/VolunteerProfileEntity.java
+++ src/main/java/com/example/demo/entity/VolunteerProfileEntity.java
@@ -26,7 +26,7 @@ public class VolunteerProfileEntity {
         this.availabilityStatus=availabilityStatus;
     }
 
-    public StudentEntity() {
+    public VolunteerProfileEntity() {
         
     }
 
