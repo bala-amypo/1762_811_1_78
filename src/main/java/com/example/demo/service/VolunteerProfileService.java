349631d0b2a070ad9e819e54df23b34f5cd255c7
bala-amypo <bala@amypo.in>
2025-12-20 16:20:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/VolunteerProfileService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.model.VolunteerProfile;
+
+import java.util.List;
+
+public interface VolunteerProfileService {
+
+    VolunteerProfile createVolunteer(VolunteerProfile volunteer);
+
+    VolunteerProfile getVolunteerById(Long id);
+
+    List<VolunteerProfile> getAllVolunteers();
+
+    VolunteerProfile updateVolunteer(Long id, VolunteerProfile volunteer);
+
+    void deleteVolunteer(Long id);
+}
