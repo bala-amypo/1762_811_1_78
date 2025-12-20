ba539fc2a713162338250347193298343c8481e5
bala-amypo <bala@amypo.in>
2025-12-20 15:06:29 +0000

    User commit

--- src/main/java/com/example/demo/service/TaskAssignmentService.java
+++ src/main/java/com/example/demo/service/TaskAssignmentService.java
@@ -1,17 +1,17 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.TaskRecordAssignmentEntity;
 import java.util.List;
+import com.example.demo.entity.TaskRecordAssignmentEntity;
 
 public interface TaskAssignmentService {
 
     TaskRecordAssignmentEntity create(TaskRecordAssignmentEntity task);
 
-    TaskRecordAssignmentEntity update(Long id, TaskRecordAssignmentEntity task);
+    List<TaskRecordAssignmentEntity> getAll();
 
     TaskRecordAssignmentEntity getById(Long id);
 
-    List<TaskRecordAssignmentEntity> getAll();
+    TaskRecordAssignmentEntity update(Long id, TaskRecordAssignmentEntity task);
 
     void delete(Long id);
 }
