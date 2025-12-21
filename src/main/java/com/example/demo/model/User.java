ac81361603a2c7a11dbc020cb3bcd8f0189ec46f
bala-amypo <bala@amypo.in>
2025-12-21 15:50:04 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -3,10 +3,7 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(
-    name = "users",
-    uniqueConstraints = @UniqueConstraint(columnNames = "email")
-)
+@Table(name = "users")
 public class User {
 
     @Id
@@ -15,13 +12,19 @@ public class User {
 
     private String fullName;
 
+    @Column(unique = true)
     private String email;
 
     private String password;
+    private String role;
 
-    private String role; // ADMIN / CUSTOMER
-
-    public User() {}
-
-    // getters and setters
+    public Long getId() { return id; }
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
