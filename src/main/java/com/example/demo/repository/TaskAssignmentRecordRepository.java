e2c432b3b9eca2b2e1bb869a34afcf918dfaa8e7
bala-amypo <bala@amypo.in>
2025-12-21 15:44:01 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/TaskAssignmentRecordRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.TaskAssignmentRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskAssignmentRecordRepository
+        extends JpaRepository<TaskAssignmentRecord, Long> {
+}
