e1473e3f116b42cfd60c2b68de4b83ab808a391a
bala-amypo <bala@amypo.in>
2025-12-21 15:48:32 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TaskAssignmentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TaskAssignmentServiceImpl.java
@@ -1,37 +1,37 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.model.TaskAssignmentRecord;
-import com.example.demo.repository.TaskAssignmentRecordRepository;
-import com.example.demo.service.TaskAssignmentService;
+import com.example.demo.model.AssignmentEvaluationRecord;
+import com.example.demo.repository.AssignmentEvaluationRecordRepository;
+import com.example.demo.service.AssignmentEvaluationService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class TaskAssignmentServiceImpl implements TaskAssignmentService {
+public class AssignmentEvaluationServiceImpl implements AssignmentEvaluationService {
 
-    private final TaskAssignmentRecordRepository repo;
+    private final AssignmentEvaluationRecordRepository repo;
 
-    public TaskAssignmentServiceImpl(TaskAssignmentRecordRepository repo) {
+    public AssignmentEvaluationServiceImpl(AssignmentEvaluationRecordRepository repo) {
         this.repo = repo;
     }
 
-    public TaskAssignmentRecord save(TaskAssignmentRecord record) {
+    public AssignmentEvaluationRecord save(AssignmentEvaluationRecord record) {
         return repo.save(record);
     }
 
-    public TaskAssignmentRecord getById(Long id) {
+    public AssignmentEvaluationRecord getById(Long id) {
         return repo.findById(id).orElseThrow();
     }
 
-    public List<TaskAssignmentRecord> getAll() {
+    public List<AssignmentEvaluationRecord> getAll() {
         return repo.findAll();
     }
 
-    public TaskAssignmentRecord update(Long id, TaskAssignmentRecord r) {
-        TaskAssignmentRecord e = getById(id);
-        e.setStatus(r.getStatus());
-        e.setNotes(r.getNotes());
+    public AssignmentEvaluationRecord update(Long id, AssignmentEvaluationRecord r) {
+        AssignmentEvaluationRecord e = getById(id);
+        e.setRating(r.getRating());
+        e.setFeedback(r.getFeedback());
         return repo.save(e);
     }
 
