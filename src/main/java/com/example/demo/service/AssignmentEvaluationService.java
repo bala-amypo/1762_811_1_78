2e32004de04bf828f7415d70941341adf86a12f6
bala-amypo <bala@amypo.in>
2025-12-26 18:14:24 +0000

    User commit

--- src/main/java/com/example/demo/service/AssignmentEvaluationService.java
+++ src/main/java/com/example/demo/service/AssignmentEvaluationService.java
@@ -1,15 +1,4 @@
-// package com.example.demo.service;
 
-// import com.example.demo.model.AssignmentEvaluationRecord;
-// import java.util.List;
-
-// public interface AssignmentEvaluationService {
-//     AssignmentEvaluationRecord save(AssignmentEvaluationRecord record);
-//     AssignmentEvaluationRecord getById(Long id);
-//     List<AssignmentEvaluationRecord> getAll();
-//     AssignmentEvaluationRecord update(Long id, AssignmentEvaluationRecord record);
-//     void delete(Long id);
-// }
 AssignmentEvaluationService.java
 package com.example.demo.service;
 import com.example.demo.model.AssignmentEvaluationRecord;
