b90f3c9b8604c54ff07f330623f6805cc3dc38b0
bala-amypo <bala@amypo.in>
2025-12-29 09:07:37 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
+++ src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
@@ -6,13 +6,3 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AssignmentEvaluationRecordRepository
         extends JpaRepository<AssignmentEvaluationRecord, Long> {
 }
-// package com.example.demo.repository;
-// import com.example.demo.model.AssignmentEvaluationRecord;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-// import java.util.List;
-// @Repository
-// public interface AssignmentEvaluationRecordRepository extends
-// JpaRepository<AssignmentEvaluationRecord, Long> {
-// List<AssignmentEvaluationRecord> findByAssignmentId(Long assignmentId);
-// }
