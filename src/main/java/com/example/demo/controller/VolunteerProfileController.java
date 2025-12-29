038d7f77971fdccc7da5858b892babdf2130b847
bala-amypo <bala@amypo.in>
2025-12-29 09:01:33 +0000

    User commit

--- src/main/java/com/example/demo/controller/VolunteerProfileController.java
+++ src/main/java/com/example/demo/controller/VolunteerProfileController.java
@@ -26,3 +26,47 @@
 // }
 
 
+package com.example.demo.controller;
+
+import com.example.demo.model.VolunteerProfile;
+import com.example.demo.service.VolunteerProfileService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/volunteers")
+public class VolunteerProfileController {
+
+    private final VolunteerProfileService service;
+
+    public VolunteerProfileController(VolunteerProfileService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public VolunteerProfile create(@RequestBody VolunteerProfile v) {
+        return service.save(v);
+    }
+
+    @GetMapping("/{id}")
+    public VolunteerProfile get(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @GetMapping
+    public List<VolunteerProfile> getAll() {
+        return service.getAll();
+    }
+
+    @PutMapping("/{id}")
+    public VolunteerProfile update(@PathVariable Long id, @RequestBody VolunteerProfile v) {
+        return service.update(id, v);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        service.delete(id);
+        return "Deleted successfully";
+    }
+}
