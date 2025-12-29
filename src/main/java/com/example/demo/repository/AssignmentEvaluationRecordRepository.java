f5593fcc2064627025e4df2bf17f559a2fee22bc
bala-amypo <bala@amypo.in>
2025-12-29 09:07:29 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
+++ src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
@@ -1,18 +1,18 @@
-// package com.example.demo.repository;
-
-// import com.example.demo.model.AssignmentEvaluationRecord;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// public interface AssignmentEvaluationRecordRepository
-//         extends JpaRepository<AssignmentEvaluationRecord, Long> {
-// }
 package com.example.demo.repository;
+
 import com.example.demo.model.AssignmentEvaluationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
-@Repository
-public interface AssignmentEvaluationRecordRepository extends
-JpaRepository<AssignmentEvaluationRecord, Long> {
-List<AssignmentEvaluationRecord> findByAssignmentId(Long assignmentId);
+
+public interface AssignmentEvaluationRecordRepository
+        extends JpaRepository<AssignmentEvaluationRecord, Long> {
 }
+// package com.example.demo.repository;
+// import com.example.demo.model.AssignmentEvaluationRecord;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+// import java.util.List;
+// @Repository
+// public interface AssignmentEvaluationRecordRepository extends
+// JpaRepository<AssignmentEvaluationRecord, Long> {
+// List<AssignmentEvaluationRecord> findByAssignmentId(Long assignmentId);
+// }
