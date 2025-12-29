245f5a3b74118793948f70fc9296fcee1edfc9ac
bala-amypo <bala@amypo.in>
2025-12-29 09:08:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/TaskRecordRepository.java
+++ src/main/java/com/example/demo/repository/TaskRecordRepository.java
@@ -1,18 +1,7 @@
-// package com.example.demo.repository;
-
-// import com.example.demo.model.TaskRecord;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// public interface TaskRecordRepository extends JpaRepository<TaskRecord, Long> {
-// }
 package com.example.demo.repository;
+
 import com.example.demo.model.TaskRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
-import java.util.Optional;
-@Repository
+
 public interface TaskRecordRepository extends JpaRepository<TaskRecord, Long> {
-List<TaskRecord> findByStatus(String status);
-Optional<TaskRecord> findByTaskCode(String taskCode);
 }
